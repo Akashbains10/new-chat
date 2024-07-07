e226eb861289247ba53bc7e80cc1570faa736884
Akash Bains <bainsakash0103@gmail.com>
2024-07-07 18:05:01 +0530

    connected socket

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
 export default function Home() {
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
+
   return null;
 }
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const http = require('http');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT ?? 3000
 const app = next({ dev: true});
 const handle = app.getRequestHandler();
+const {socketService} = require('./services/socketService')
 
 app.prepare().then(() => {
     const server = express();
-
+    const httpServer = http.createServer(server);
+    socketService(httpServer);
     server.get('/dummy', (req, res) => {
         try {
             return res.send('This is an custom server')
@@ -19,7 +22,7 @@ app.prepare().then(() => {
         return handle(req, res);
     });
 
-    server.listen(port, (err) => {
+    httpServer.listen(port, (err) => {
         if (err) throw err;
         console.log(`- Local:      http://localhost:${port}`)
     })
